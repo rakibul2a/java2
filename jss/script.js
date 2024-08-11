5ba7d7218f90d60aabaa5d2c18becf6bcf9be2d1
Rakibul Islam <167644021+rakibul2a@users.noreply.github.com>
2024-08-11 18:55:20 +0600

    Add files via upload

---
+++ jss/script.js
@@ -0,0 +1,68 @@
+// e1
+function showText(){
+	document.getElementById('s_text').innerHTML="Hello SSB"
+}
+
+// e2
+function showTime(){
+	document.getElementById('s_time').innerHTML = Date()
+}
+
+// e3
+function java(){
+	document.getElementById('demo').innerHTML = 'Hello JavaScript!'
+}
+
+// e4
+function bulbOn(){
+	document.getElementById('img').src='images/on.gif'
+
+}
+// e4
+function bulbOff(){
+	document.getElementById('img').src='images/off.gif'
+}
+
+// e5
+function size(){
+	document.getElementById('s_size').style.fontSize='35px'
+}
+// e6
+function hide(){
+	document.getElementById('s_hide').style.display='none'
+}
+// e7
+function show(){
+	document.getElementById('s_show').innerHTML='people was independent'
+}
+// e8
+function showView(){
+	document.getElementById()
+}
+
+// e9
+function myFunction() {
+  document.getElementById("new").innerHTML = "Paragraph changed.";
+}
+
+// e10
+function showSum() {
+  document.getElementById("sum").innerHTML = 5 + 6;
+}
+// e11
+function showSub() {
+  document.getElementById("sub").innerHTML = 6 - 1;
+}
+
+// e13
+function state() {
+let x, y, z;  
+x = 5;        
+y = 6;        
+z = x + y; 
+
+  document.getElementById("state").innerHTML ="The value of z is " + z + ".";  
+}
+
+
+
